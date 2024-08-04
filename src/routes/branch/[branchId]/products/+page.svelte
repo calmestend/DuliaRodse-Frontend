7a6b64b84be82f1117b2dc5a910b8d2d110d3416
Barac Fabregat <calmestend@gmail.com>
2024-08-03 19:24:47 -0600

    Add if branch exist verification on branch selection

--- src/routes/branch/[branchId]/products/+page.svelte
+++ src/routes/branch/[branchId]/products/+page.svelte
@@ -2,7 +2,7 @@
 	import { goto } from '$app/navigation';
 	import { page } from '$app/stores';
 	import AddProduct from '$lib/components/AddProduct.svelte';
-	import { productsInventoryStores } from '$lib/inventory/stores';
+	import { branchesStores, productsInventoryStores } from '$lib/inventory/stores';
 	import type { PageData } from './$types';
 
 	export let data: PageData;
@@ -10,17 +10,21 @@
 	const branchId = $page.params.branchId;
 </script>
 
-{#each $productsInventoryStores as product}
-	{#if product.branchId === parseInt(branchId) && product.active && product.existence > 0}
-		<h2>{product.name}</h2>
-		<p>Sucursal: {product.branchId}</p>
-		<p>Numero de inventario: {product.inventoryId}</p>
-		<hr />
-		{#if data.authenticated && data.admin === false}
-			<AddProduct {product} />
+{#if $branchesStores.find((branch) => branch.id === parseInt(branchId))}
+	{#each $productsInventoryStores as product}
+		{#if product.branchId === parseInt(branchId) && product.active && product.existence > 0}
+			<h2>{product.name}</h2>
+			<p>Sucursal: {product.branchId}</p>
+			<p>Numero de inventario: {product.inventoryId}</p>
+			<hr />
+			{#if data.authenticated && data.admin === false}
+				<AddProduct {product} />
+			{/if}
+			<button on:click={() => goto(`/branch/${branchId}/products/${product.inventoryId}`)}
+				>Ver Mas</button
+			>
 		{/if}
-		<button on:click={() => goto(`/branch/${branchId}/products/${product.inventoryId}`)}
-			>Ver Mas</button
-		>
-	{/if}
-{/each}
+	{/each}
+{:else}
+	<h1>Sucursal inexistente o inactiva</h1>
+{/if}
