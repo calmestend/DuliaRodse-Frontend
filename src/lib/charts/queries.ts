d10cf774dd315ae5475630c7eae95f51213c55b2
Barac Fabregat <calmestend@gmail.com>
2024-08-06 01:39:41 -0600

    Add Report charts

---
+++ src/lib/charts/queries.ts
@@ -0,0 +1,57 @@
+import { fillShoppingHistory, shoppingHistory } from '$lib/shopping/stores';
+import { createChart } from 'lightweight-charts';
+import { get } from 'svelte/store';
+
+export async function purchaseHistory(domElement: HTMLElement) {
+	shoppingHistory.set([]);
+	await fillShoppingHistory();
+
+	const purchases = get(shoppingHistory);
+
+	// Agrupar y sumar los montos por fecha
+	const aggregatedData = purchases.reduce((acc, purchase) => {
+		const [year, month, day] = purchase.saleDate.split('-');
+		const dateKey = `${year}-${month}-${day}`;
+
+		if (!acc[dateKey]) {
+			acc[dateKey] = { date: dateKey, amount: 0 };
+		}
+
+		acc[dateKey].amount += parseFloat(purchase.amount);
+		return acc;
+	}, {});
+
+	// Convertir el objeto de datos agregados en un array adecuado para el gráfico
+	const data = Object.values(aggregatedData).map(({ date, amount }) => {
+		const [year, month, day] = date.split('-');
+		return {
+			time: new Date(year, month - 1, day).getTime() / 1000, // Convertir a timestamp UNIX en segundos
+			value: amount // Sumar el monto para la fecha
+		};
+	});
+
+	// Crear el gráfico
+	const chart = createChart(domElement, { width: 600, height: 400 });
+	const series = chart.addLineSeries({
+		color: '#2962FF',
+		lineWidth: 2,
+		// disabling built-in price lines
+		lastValueVisible: false,
+		priceLineVisible: false
+	});
+
+	// Establecer los datos en la serie de líneas
+	series.setData(data);
+
+	// Opcional: Añadir etiquetas personalizadas para las fechas en el eje X
+	chart.applyOptions({
+		timeScale: {
+			tickMarkFormatter: (timestamp) => {
+				const date = new Date(timestamp * 1000);
+				return `${date.getDate()}/${date.getMonth() + 1}/${date.getFullYear()}`;
+			}
+		}
+	});
+
+	chart.timeScale().fitContent();
+}
--- src/routes/admin/+page.svelte
+++ src/routes/admin/+page.svelte
@@ -1,1 +1,14 @@
+<script lang="ts">
+	import { purchaseHistory } from '$lib/charts/queries';
+	import { onMount } from 'svelte';
+
+	let chartContainer: HTMLElement;
+
+	onMount(() => {
+		purchaseHistory(chartContainer);
+	});
+</script>
+
 <h1>Reportes</h1>
+<h2>Fecha x Total Vendido</h2>
+<div id="container" bind:this={chartContainer}></div>
